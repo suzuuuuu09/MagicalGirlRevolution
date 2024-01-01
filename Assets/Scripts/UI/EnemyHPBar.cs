2a9077f14a2eaf8de12a07d28007cd8692aa3794
suzuuuuu09 <bsamu5788@gmail.com>
2024-01-01 21:07:43 +0900

    あああ

--- Assets/Scripts/UI/EnemyHPBar.cs
+++ Assets/Scripts/UI/EnemyHPBar.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class EnemyHPBar : MonoBehaviour
 {
--- Assets/modern-gui-pixel-art/readme.txt
+++
@@ -1,2 +0,0 @@
-Font Terminal
-http://www.softpedia.com/get/Others/Font-Utils/Terminal-Font.shtml
